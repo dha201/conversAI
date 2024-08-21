78202265784453e3cccac9429e56342fe6c6ffb8
dha201 <davidmha2001@gmail.com>
2024-08-21 12:26:21 -0400

    Vercel analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <ClerkProvider publishableKey='pk_test_bW9kZXN0LXBob2VuaXgtNzUuY2xlcmsuYWNjb3VudHMuZGV2JA'>
       <html lang="en" className={inter.className}>
-        <body className="bg-white flex flex-col gap-4">{children}</body>
+        <body className="bg-white flex flex-col gap-4">
+          {children}
+          <Analytics />  
+        </body>
       </html>
     </ClerkProvider>
   );
